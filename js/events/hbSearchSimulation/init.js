a6710d70ea8684242d1d28cc8c79d8bea4dcbb63
Adam Cameron <adam.cameron@hostelbookers.com>
2015-07-08 17:00:56 +0100

    JS: HB stuff

---
+++ js/events/hbSearchSimulation/init.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	var page = new Page("#page");
+	var sortControls = new Sorter("#page");
+	var filterControls = new Filters("#page");
+	var paginationControls = new Pagination("#paginationControls", "#pageSizeControls");
+	var pageSizeControls = new Page("#page");
+});
