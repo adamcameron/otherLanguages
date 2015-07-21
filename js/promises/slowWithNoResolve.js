b10ddcb385c1cf47e00593e100f366d44d70cab3
Adam Cameron <adam.cameron@hostelbookers.com>
2015-07-21 08:50:33 +0100

    JS/Promise: adding slowWithNoResolve example

--- js/promises/slowWithNoResolve.js
+++ js/promises/slowWithNoResolve.js
@@ -1,20 +1,24 @@
 // slowWithNoResolve.js
 
+
 slowThing = function(label){
 	console.log(label);
 	console.log("Beginning of slowThing(\"" + label + "\") process");
-	startTime=new Date();
+	var startTime=new Date();
 	var words = ulyssesExtract.replace(/(\b\w+\b)(?=.*\1)/gi, "").replace(/\s+/g," ");
-	endTime = new Date();
-	elapsed = endTime - startTime;
+	var endTime = new Date();
+	var elapsed = endTime - startTime;
 	console.log("After slowThing(\"" + label + "\") process (" + elapsed + "ms)\n\n\n");
 };
 
 
+var startTime=new Date();
 console.log("Before promise");
 new Promise(
 	function(resolve, reject) {
 		slowThing("Executor");
 	}
 );
-console.log("After promise");
+var endTime = new Date();
+var elapsed = endTime - startTime;
+console.log("After promise (" + elapsed + "ms)\n\n\n");
