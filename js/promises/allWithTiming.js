16be1e29cf54d9b5b30772339c06068d5bcde13e
Adam Cameron <adam.cameron@hostelbookers.com>
2015-07-23 14:49:08 +0100

    JS/Promises: all / race

--- js/promises/allWithTiming.js
+++ js/promises/allWithTiming.js
@@ -1,4 +1,10 @@
 // all.js
+startTime = new Date();
+
+function getElapsed(){
+	return new Date() - startTime;	
+}
+
 
 promiseGenerator = {};
 promiseGenerator[Symbol.iterator] = function*(){
@@ -12,21 +18,21 @@ promiseGenerator[Symbol.iterator] = function*(){
 
 getIndexedPromise = function(index){
 	return new Promise(function(resolve,reject){
-		console.log("(" + index + ") Promise executor");
+		console.log("(" + index + ") Promise executor @ " + getElapsed());
 		resolve(index);
 	}).then(function(value){
-		slowThing("(" + value + ") then() resolver");
+		slowThing("(" + value + ") then() resolver @ " + getElapsed());
 		return Promise.resolve(value);
 	});
 };
 
-console.log("Before finalPromise is created");
+console.log("Before finalPromise is created @ " +getElapsed());
 finalPromise = Promise.all(promiseGenerator);
 
 console.log("Before finalPromise has then() attached");
 finalPromise.then(function(value){
 	console.dir(value);
-	console.log("finalPromise's then()'s resolve() called with " + value);
+	console.log("finalPromise's then()'s resolve() called with " + value +  " @ " + getElapsed());
 });
 console.log("End of processing");
 
