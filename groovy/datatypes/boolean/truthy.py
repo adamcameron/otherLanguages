89f51c3ff73d92d9c2104ee132ddf54628dbf233
Adam Cameron <dac.cfml@gmail.com>
2016-01-30 22:27:22 +0000

    fortran???

--- groovy/datatypes/boolean/truthy.py
+++ groovy/datatypes/boolean/truthy.py
@@ -64,3 +64,24 @@ else:
     print("falsy")
 print("=====================")
 
+class Test:
+    def __init__(self, value):
+        self.value = value
+
+    def __bool__(self):
+        return self.value == "truthy"
+
+print("truthy object")
+test = Test("truthy")
+if test:
+    print("truthy")
+else:
+    print("falsy")
+print("=====================")
+
+print("non-truthy object")
+if Test("anything else"):
+    print("truthy")
+else:
+    print("falsy")
+print("=====================")
