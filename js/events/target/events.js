6d7fbb3385eed780cc2f08b0da0751434dfe79ed
Adam Cameron <adam.cameron@hostelworld.com>
2016-03-17 08:39:20 +0000

    JS: proxy

--- js/events/target/events.js
+++ js/events/target/events.js
@@ -3,17 +3,34 @@ Page = function(page){
 };
 
 Page.bindHandlers = function(page){
-	$("#btn1").on("click", $.proxy(page.clickHandlerUsingProxy, page));
-	$("#btn2").on("click", page.clickHandlerWithoutProxy);
+	$("#noProxy").on("click", page.clickHandlerWithoutProxy);
+	$("#proxyRef").on("click", page.clickHandlerWithProxyRef());
+	$("#jqueryProxy").on("click", $.proxy(page.clickHandlerUsingJQueryProxy, page));
+	$("#usingBind").on("click", page.clickHandlerUsingBind.bind(page));
 };
 
-Page.prototype.clickHandlerUsingProxy = function(e){
-	console.log("'e' refers to the object triggering the event: " + $(e.target).text());
-	console.log("'this' still refers to the object: " + this.page);
-};
 Page.prototype.clickHandlerWithoutProxy = function(e){
-	console.log("'e' refers to the object triggering the event: " + $(e.target).text());
-	console.log("'this' also refers to the object triggering the event: " + $(this).text());
+	console.log("'e.target' refers to the button that triggered the event: " + $(e.target).text());
+	console.log("'this' also refers to the button that triggered the event: " + $(this).text());
+	console.log("Cannot reference 'this' from the Page object");
+};
+
+Page.prototype.clickHandlerWithProxyRef = function(){
+	var self = this;
+	return function(e){
+		console.log("'this' refers to the button that triggered the event: " + $(this).text());
+		console.log("'self' still refers to the Page object: " + self.page);
+	}
+};
+
+Page.prototype.clickHandlerUsingJQueryProxy = function(e){
+	console.log("'e' refers to the button that triggered the event: " + $(e.target).text());
+	console.log("'this' still refers to the Page object: " + this.page);
+};
+
+Page.prototype.clickHandlerUsingBind = function(e){
+	console.log("'e' refers to the button that triggered the event: " + $(e.target).text());
+	console.log("'this' still refers to the Page object: " + this.page);
 };
 
 $(document).ready(function(){
