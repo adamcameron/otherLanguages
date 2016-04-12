f6c0f40a6225f6224acc023a72a9b82bb32af2d9
Adam Cameron <adam.cameron@hostelworld.com>
2016-04-12 16:08:48 +0100

    JS: using bind to effect closure

---
+++ js/closure/usingBind.js
@@ -0,0 +1,55 @@
+console.log("Working example using inline function and implicit closure");
+var usingInline = function(firstThree, fourth, type){
+	var allFour = firstThree;
+	allFour.push(fourth);
+	var prefix = type; // defined in calling context
+	allFour.forEach(function(element){
+		console.log(prefix + ": " + element); // prefix available due to closure
+	});
+};
+
+var oneTwoThreeInMaori = ["tahi", "rua", "toru"];
+var fourInMaori = "wha";
+usingInline(oneTwoThreeInMaori, fourInMaori, "Number");
+console.log("\n\n\n\n");
+
+
+
+console.log("Working example using bind to implement closure explicitly");
+var usingExtractedWithBind = function(firstThree, fourth, type){
+	var allFour = firstThree;
+	allFour.push(fourth);
+	var prefix = type;
+	
+	allFour.forEach(eachHandlerToUseWithBind.bind(undefined, prefix)); // bind prefix to function
+};
+
+var eachHandlerToUseWithBind = function(prefix, element){ // prefix is the FIRST argument to the callback, then the normal args that forEach() passes
+	console.log(prefix + ": " + element);
+};
+
+
+var oneTwoThreeInMaori = ["tahi", "rua", "toru"];
+var fourInMaori = "wha";
+usingExtractedWithBind(oneTwoThreeInMaori, fourInMaori, "Number");
+console.log("\n\n\n\n");
+
+
+
+console.log("Broken example");
+var usingExtractedWithoutBind = function(firstThree, fourth, type){
+	var allFour = firstThree;
+	allFour.push(fourth);
+	var prefix = type;
+	
+	allFour.forEach(eachHandler);
+};
+
+var eachHandler = function(element){
+	console.log(prefix + ": " + element); // prefix is not defined in this context, so this will break
+};
+
+var oneTwoThreeInMaori = ["tahi", "rua", "toru"];
+var fourInMaori = "wha";
+usingExtractedWithoutBind(oneTwoThreeInMaori, fourInMaori, "Number");
+console.log("\n\n\n\n");
