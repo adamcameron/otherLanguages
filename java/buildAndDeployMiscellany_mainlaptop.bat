6fbdf4af5fa36fffeda7861d1a0556978ee88623
Adam Cameron <cameron.adam@gmail.com>
2017-07-28 18:36:41 +0100

    sync

--- java/buildAndDeployMiscellany_mainlaptop.bat
+++ java/buildAndDeployMiscellany_mainlaptop.bat
@@ -1,5 +1,6 @@
 javac me\adamcameron\miscellany\*.java
 jar -cf miscellany.jar me\adamcameron\miscellany\*.class
 copy miscellany.jar D:\apps\adobe\ColdFusion\11\express\cfusion\wwwroot\WEB-INF\lib
+copy miscellany.jar D:\apps\adobe\ColdFusion\2016\express\cfusion\wwwroot\WEB-INF\lib
 copy miscellany.jar D:\apps\lucee\4.5\express\webapps\ROOT\WEB-INF\lucee\lib
 copy miscellany.jar D:\apps\lucee\5.0\express\webapps\ROOT\WEB-INF\lucee\lib
